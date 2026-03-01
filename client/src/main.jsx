eb752ec024cde84d78a65a3bd7842372fa614022
seenalwatw <seenlawatw.sandbox@gmail.com>
2026-03-01 21:12:04 +0700

    feat: add dockerfile, docker-compose, and nginx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,8 +6,8 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/booking-calendar">
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );
